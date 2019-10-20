4d0bf92df826f0aa830e5ab8d72126d9094dcfbd
Sébastien Blin <sebastien.blin@savoirfairelinux.com>
2019-10-20 18:27:07 -0400

    tsui: start interface

--- src/main.rs
+++ src/main.rs
@@ -50,6 +50,138 @@ use std::sync::{Arc, Mutex};
 use std::thread;
 use futures::sync::mpsc;
 
+
+extern crate failure;
+extern crate tui;
+extern crate termion;
+
+
+#[allow(dead_code)]
+mod util;
+
+use std::io;
+use std::time::Duration;
+
+use termion::event::Key;
+use termion::input::MouseTerminal;
+use termion::raw::IntoRawMode;
+use termion::screen::AlternateScreen;
+use tui::backend::TermionBackend;
+use tui::layout::{Constraint, Direction, Layout, Rect};
+use tui::style::Color;
+use tui::widgets::canvas::{Canvas, Map, MapResolution, Rectangle};
+use tui::widgets::{Block, Borders, Widget};
+use tui::Terminal;
+
+use util::{Config, Event, Events};
+
+struct App {
+    player: Rect,
+}
+
+impl App {
+    fn new() -> App {
+        App {
+            player: Rect::new(0, 0, 0, 0),
+        }
+    }
+
+    fn update(&mut self) {
+    }
+}
+
+fn main() -> Result<(), failure::Error> {
+    // Terminal initialization
+    let stdout = io::stdout().into_raw_mode()?;
+    let stdout = MouseTerminal::from(stdout);
+    let stdout = AlternateScreen::from(stdout);
+    let backend = TermionBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+    terminal.hide_cursor()?;
+
+    // Setup event handlers
+    let config = Config {
+        tick_rate: Duration::from_millis(100),
+        ..Default::default()
+    };
+    let events = Events::with_config(config);
+
+    // App
+    let mut app = App::new();
+
+    loop {
+        terminal.draw(|mut f| {
+            let size = f.size();
+            let w = size.width / (13 + 2);
+            let h = size.height / (13 + 2);
+            let square_size = std::cmp::min(w, h);
+            Canvas::default()
+                .block(Block::default().borders(Borders::ALL).title("Bomberust"))
+                .paint(|ctx| {
+                    // TODO size
+                    let offset_x = ((((size.width as f32 / square_size as f32) - 13 as f32)  * square_size as f32) / 2.0) as u16;
+                    let offset_y = ((((size.height as f32 / square_size as f32) - 13 as f32)  * square_size as f32) / 2.0) as u16;
+                    for x in 0..13 {
+                        for y in 0..13 {
+                            ctx.draw(&Rectangle {
+                                rect: Rect::new(x * square_size + offset_x, y * square_size + offset_y, square_size, square_size),
+                                color: Color::Yellow,
+                            });
+                        }
+                    }
+                    ctx.draw(&Rectangle {
+                        rect: Rect::new(offset_x - square_size,
+                                        offset_y - square_size,
+                                        (13 + 2) * square_size,
+                                        (13 + 2) * square_size),
+                        color: Color::Red,
+                    });
+
+                    ctx.draw(&Rectangle {
+                        rect: app.player,
+                        color: Color::Blue,
+                    });
+                })
+                .x_bounds([0.0, size.width as f64])
+                .y_bounds([0.0, size.height as f64])
+                .render(&mut f, size);
+            app.player.width = std::cmp::max(2, square_size - 2);
+            app.player.height = std::cmp::max(2, square_size - 2);
+        })?;
+
+
+        match events.next()? {
+            Event::Input(input) => match input {
+                Key::Char('q') => {
+                    break;
+                }
+                Key::Down => {
+                    app.player.y -= 1;
+                }
+                Key::Up => {
+                    app.player.y += 1;
+                }
+                Key::Right => {
+                    app.player.x += 1;
+                }
+                Key::Left => {
+                    app.player.x -= 1;
+                }
+
+                _ => {}
+            },
+            Event::Tick => {
+                app.update();
+            }
+        }
+    }
+
+    Ok(())
+}
+
+
+
+/*
 fn main() {
     // Init logging
     env_logger::init();
@@ -77,4 +209,4 @@ fn main() {
 
     let _ = client_thread.join();
     let _ = key_handler_thread.join();
-}
+}*/
